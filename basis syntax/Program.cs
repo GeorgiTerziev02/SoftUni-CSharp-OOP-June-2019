8eb683fea9503d0864dff3f0b7928950304b058f
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2019-07-28 17:34:44 +0300

    Add files via upload

---
+++ basis syntax/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basis_syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int x;
+            if (a < b) { x = a; a = b; b = x; }
+            if (a < c) { x = a; a = c; c = x; }
+            if (b < c) { x = b; b = c; c = x; }
+
+
+            Console.WriteLine(a);Console.WriteLine(b);Console.WriteLine(c);
+
+
+
+
+
+
+        }
+    }
+}
