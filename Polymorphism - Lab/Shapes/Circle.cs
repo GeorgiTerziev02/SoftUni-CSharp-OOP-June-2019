9c1d94026e4ad5154833b35c6408f93b0ad7068f
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2019-08-09 00:28:23 +0300

    Add files via upload

---
+++ Polymorphism - Lab/Shapes/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override double CalculateArea()
+        {
+            return this.radius * this.radius * Math.PI;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return this.radius * 2 * Math.PI;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + "Circle";
+        }
+    }
+}
