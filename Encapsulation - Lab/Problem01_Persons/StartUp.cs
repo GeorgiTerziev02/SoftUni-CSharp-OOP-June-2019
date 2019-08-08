eb3307a93e0e2ba4bf9c5b5e4b987c54a522ada8
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2019-08-08 09:53:22 +0300

    Add files via upload

---
+++ Encapsulation - Lab/Problem01_Persons/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            var lines = int.Parse(Console.ReadLine());
+            var people = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var currentPerson = Console.ReadLine().Split();
+                try
+                {
+                    var person = new Person(currentPerson[0], currentPerson[1], int.Parse(currentPerson[2]), decimal.Parse(currentPerson[3]));
+                    people.Add(person);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Team team = new Team("SoftUni");
+
+            foreach (var person in people)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+        }
+    }
+}
---
+++ Encapsulation - Lab/Problem01_Persons/Team.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public IReadOnlyCollection<Person> FirstTeam
+        {
+            get
+            {
+                return this.firstTeam.AsReadOnly();
+            }
+        }
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get
+            {
+                return this.reserveTeam.AsReadOnly();
+            }
+        }
+
+        public Team(string name)
+        {
+            this.name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+        public void AddPlayer(Person person)
+        {
+            if(person.Age<40)
+            {
+                this.firstTeam.Add(person);
+            }
+            else
+            {
+                this.reserveTeam.Add(person);
+            }
+        }
+    }
+}
